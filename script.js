9992186ed3d4647ab2ee6e09cf4e13f574659795
LUCAS IMPERATORE CASTRO DOS SANTOS <lucasimperatore@gmail.com>
2025-06-18 11:38:37 -0300

    versão atualizada

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-let lista= []
-let task= document.getElementById("task")
-function add(){
-    let tarefas= document.getElementById("tarefa").value
-    lista.push(tarefas) 
-    console.log(lista)
-for(let i=0; i<= lista.length; i++){
-    let item = document.createElement("li")
-    item.textContent = lista[i]
-task.appendChild(item)
-}
+const taskInput = document.getElementById("taskInput")
+const taskList = document.getElementById("taskList")
+
+function addTask(){
+    const taskText = taskInput.value.trim()
+    if(taskText!==""){
+        const listItem = document.createElement("li")
+        listItem.innerHTML= `<span>${taskText}</span>  <button class="remove-btn" 
+        onclick="removeTask(this)">excluir</button>`
+        
+        listItem.addEventListener("click", function (event) {
+          
+          if (!event.target.classList.contains("remove-btn")) {
+            listItem.style.textDecoration = "line-through";
+            listItem.style.color = "green";
+          }
+        })
     
+        taskList.appendChild(listItem)
+        taskInput.value= ""
+    }else{
+        alert("digite uma tarefa para adicionar à lista!")
+    }
+
+
 }
+
+
+
+function removeTask(taskElement){
+    taskElement.parentElement.remove()
+}
+
+
+
+
+
+
 
